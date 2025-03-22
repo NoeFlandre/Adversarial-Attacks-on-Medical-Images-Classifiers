4509689921f095972359c881be2baa65d0da806c
NoeFlandre <noeflandre@gmail.com>
2025-03-22 20:06:44 +0530

    Implement a dataloader

---
+++ models/logistic_classifier/dataset.py
@@ -0,0 +1,36 @@
+import os
+from torch.utils.data import Dataset
+from PIL import Image
+import torchvision.transforms as transforms
+
+class BreastHistopathologyDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.image_paths = []
+        self.labels = []
+
+        for patient_id in os.listdir(root_dir):
+            patient_folder = os.path.join(root_dir, patient_id)
+            if not os.path.isdir(patient_folder):
+                continue
+
+            for class_label in ['0', '1']:
+                class_folder = os.path.join(patient_folder, class_label)
+                if not os.path.exists(class_folder):
+                    continue
+                for img_name in os.listdir(class_folder):
+                    if img_name.endswith('.png'):
+                        self.image_paths.append(os.path.join(class_folder, img_name))
+                        self.labels.append(int(class_label))
+
+        self.transform = transform or transforms.Compose([
+            transforms.Resize((50, 50)),
+            transforms.ToTensor()
+        ])
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image = Image.open(self.image_paths[idx]).convert('RGB')
+        label = self.labels[idx]
+        return self.transform(image), label
